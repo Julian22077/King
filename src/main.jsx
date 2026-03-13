f402e1205d3424d9b15584cba1bad7e68506f0ba
Julian22077 <camilolozano0122@gmail.com>
2026-03-13 03:27:15 +0000

    segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Game from './Game'
+import './Game.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Game />
   </StrictMode>,
 )
